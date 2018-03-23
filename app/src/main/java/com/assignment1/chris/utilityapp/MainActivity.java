7f25440a35a8ff66f75393acd647074c2211003d
chris-breed <christopher.breed@my.jcu.edu.au>
2018-03-23 23:20:49 +1000

    touch event gesture

--- app/src/main/java/com/assignment1/chris/utilityapp/MainActivity.java
+++ app/src/main/java/com/assignment1/chris/utilityapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.assignment1.chris.utilityapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -24,7 +26,7 @@ import org.json.JSONObject;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity /*implements GestureDetector.OnGestureListener */ {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     double conversion;
     EditText editTop;
@@ -32,18 +34,21 @@ public class MainActivity extends AppCompatActivity /*implements GestureDetector
     Spinner spinnerTop;
     Spinner spinnerBottom;
 
+    final String url = "https://free.currencyconverterapi.com/api/v5/convert?q=";
+    private GestureDetectorCompat detector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        detector = new GestureDetectorCompat(this, this);
 
         String defaultCurrency = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
                 .getString("defaultCurrency", "falseValue");
         String previousAmount = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
                 .getString("previousAmount", "falseValue");
 
-        final String url = "https://free.currencyconverterapi.com/api/v5/convert?q=";
 
         // Conversion Texts
         editTop = findViewById(R.id.edit_top);
@@ -96,76 +101,6 @@ public class MainActivity extends AppCompatActivity /*implements GestureDetector
             }
         });
 
-        Button convert = findViewById(R.id.btn_convert);
-        convert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Send request for conversion
-
-                editBottom.setText(R.string.txt_converting);
-
-                final String to = spinnerBottom.getSelectedItem().toString().split(",")[0];
-                final String from = spinnerTop.getSelectedItem().toString().split(",")[0];
-//                Log.i("Volley", "Converting from: " + from + " to " + to + ".");
-
-                String newUrl = generateURL(url, from, to);
-
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-
-                StringRequest stringRequest = new StringRequest(
-                        Request.Method.GET, newUrl, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-//                        Log.i("Volley", response);
-
-                        try {
-                            conversion = getConversion(to, from, response);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                        double convertedValue = convertValue(conversion);
-
-                        editBottom.setText(correctStringForEditText(convertedValue));
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-//                        Log.i("Volley", "onErrorResponse() called");
-                    }
-                });
-                queue.add(stringRequest);
-            }
-
-
-            private double getConversion(String to, String from, String response) throws JSONException {
-                JSONObject json = new JSONObject(response);
-                try {
-                    conversion = json.getJSONObject("results").getJSONObject((from + "_" + to)
-                            .toUpperCase()).getDouble("val");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-//                Log.i("Volley", conversion + "");
-
-                return conversion;
-            }
-
-            private String generateURL(String url, String from, String to) {
-                return (url + from.toUpperCase() + "_" + to.toUpperCase());
-            }
-
-            private double convertValue(double conversion) {
-                // Converts the value of the editText to be equal to conversion * value
-                float originalValue = Float.parseFloat(editTop.getText().toString());
-                return originalValue * conversion;
-            }
-
-            private String correctStringForEditText(double val) { // Readies the value to be inserted into TextView
-                float decimalRemoved = Float.parseFloat(String.format(Locale.ENGLISH, "%.2f", val));
-                return String.format("$%s", decimalRemoved);
-            }
-        });
 
         Button settings = findViewById(R.id.btn_config);
         settings.setOnClickListener(new View.OnClickListener() {
@@ -185,5 +120,107 @@ public class MainActivity extends AppCompatActivity /*implements GestureDetector
         PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit()
                 .putString("previousAmount", editTop.getText().toString()).apply();
 
+    }
+
+    private String generateURL(String url, String from, String to) {
+        return (url + from.toUpperCase() + "_" + to.toUpperCase());
+    }
+
+    private double convertValue(double conversion) {
+        // Converts the value of the editText to be equal to conversion * value
+        float originalValue = Float.parseFloat(editTop.getText().toString());
+        return originalValue * conversion;
+    }
+
+    private String correctStringForEditText(double val) { // Readies the value to be inserted into TextView
+        float decimalRemoved = Float.parseFloat(String.format(Locale.ENGLISH, "%.2f", val));
+        return String.format("$%s", decimalRemoved);
+    }
+
+    private double getConversion(String to, String from, String response) throws JSONException {
+        JSONObject json = new JSONObject(response);
+        try {
+            conversion = json.getJSONObject("results").getJSONObject((from + "_" + to)
+                    .toUpperCase()).getDouble("val");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//                Log.i("Volley", conversion + "");
+
+        return conversion;
+    }
+
+    public void convertButtonPress(View view) {
+        editBottom.setText(R.string.txt_converting);
+
+        final String to = spinnerBottom.getSelectedItem().toString().split(",")[0];
+        final String from = spinnerTop.getSelectedItem().toString().split(",")[0];
+//                Log.i("Volley", "Converting from: " + from + " to " + to + ".");
+
+        String newUrl = generateURL(url, from, to);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET, newUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                        Log.i("Volley", response);
+
+                try {
+                    conversion = getConversion(to, from, response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                double convertedValue = convertValue(conversion);
+
+                editBottom.setText(correctStringForEditText(convertedValue));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                        Log.i("Volley", "onErrorResponse() called");
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        convertButtonPress(findViewById(android.R.id.content));
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
